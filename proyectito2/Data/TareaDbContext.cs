6170f0a98e852f44b9a4ee2088c98461382d8f68
fabiCas-D-04 <fcd040206@gmail.com>
2025-08-26 21:02:40 -0400

    segundo commit a medias

---
+++ proyectito2/Data/TareaDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using proyectito2.Models;
+
+
+namespace WARazorDB.Data
+{
+    public class TareaDbContext : DbContext
+    {
+        public TareaDbContext(DbContextOptions<TareaDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Tarea> Tareas { get; set; }
+        protected TareaDbContext()
+        {
+
+        }
+    }
+}
---
+++ proyectito2/Models/Tarea.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+
+namespace proyectito2.Models
+{
+    public class Tarea
+    {
+        public int idTarea {  get; set; }
+
+        public string nombreTarea { get; set; }
+
+        public DateTime fechaVencimiento { get; set; }
+
+        public string estado { get; set; }
+
+        public int idUsuario { get; set; }
+    }
+}
